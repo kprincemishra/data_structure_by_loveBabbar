eb1580c8950710bb5939d6f1374a6999d1337f17
Prince Kumar Mishra <kprincemishra@gmail.com>
2024-03-10 02:52:35 +0530

    recursion

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+    //base case
+    if(n == 1 || n == 0)
+    return 1;
+    // //recursive relation
+    // int ans = n * fact(n - 1);
+    // return ans;
+    //recursive relation
+    int recursiveAns = fact(n - 1);
+    //processing
+    int ans = n * recursiveAns;
+    return ans;
+}
+int main()
+{
+    cout<<fact(5);
+    return 0;
+}
